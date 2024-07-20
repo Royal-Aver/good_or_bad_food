1040337d788292c170c93e4e3dd0e169a1b2ec1e
Stas Savelev <st4syan1@yandex.ru>
2024-07-20 12:22:08 +0300

    add urls & views, setup admin-zone

---
+++ good_or_bad_food/food/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'food'
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path(
+        'category/<slug:category_slug>/',
+        views.category_product,
+        name='category_slug'),
+    path(
+        'product/<slug:product_slug>',
+        views.product_detail,
+        name='product_slug'),
+]
