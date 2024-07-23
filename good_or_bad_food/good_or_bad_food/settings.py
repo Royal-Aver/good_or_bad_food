b736d34fdcc50232fb6f2f30a45f8fa21a5ab288
kram1k <7685181@gmail.com>
2024-07-23 16:43:08 +0300

    Fix templates, change carousel color on black

--- good_or_bad_food/good_or_bad_food/settings.py
+++ good_or_bad_food/good_or_bad_food/settings.py
@@ -23,8 +23,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    'django_bootstrap5',
 
-    # 'bootstrap5',
     'debug_toolbar',
 
     'food.apps.FoodConfig',
